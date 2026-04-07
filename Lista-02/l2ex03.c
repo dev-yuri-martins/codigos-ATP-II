36f636c083e4d33666d5911843c75ce7e384e2b6
Yuri-silva-dev <yurimartins.dev@yahoo.com>
2026-04-07 16:21:49 -0300

    Exercicio-03

---
+++ Lista-02/l2ex03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void numero(int *x) {
+    *x *= 2;
+    printf("Pós função: %i\n", *x);
+}
+
+int main() {
+    int a;
+    scanf(" %d", &a);
+    
+    printf("Original: %d\n", a);
+    
+    numero(&a);
+    
+    printf("Original de novo: %d\n", a);
+    return 0;
+}
