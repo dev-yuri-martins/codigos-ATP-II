1f218db1e863cb60db03ce76ece119aef0d44bde
Yuri-silva-dev <yurimartins.dev@yahoo.com>
2026-04-07 16:22:51 -0300

    Exercicio-05

---
+++ Lista-02/l2ex05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    scanf(" %d\n", &x);
+    
+    if (x % 2 == 0) {
+     int y 
+    }
+    
+    printf(" %d\n", y);
+    
+    return 0;
+    //Se o objetivo da questão era mostrar que dá erro de compilação então está certa
+}
